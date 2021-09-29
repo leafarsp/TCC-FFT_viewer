a74726c080d5ce3c3b119da9f3e14aeac0e31908
leafarsp <leafarsp@gmail.com>
2021-09-29 11:09:39 -0300

    Criada branch para exibição da FFT da velocidade

--- FFT_viewer.py
+++ FFT_viewer.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sys
+from datetime import datetime
 import re
 
 
@@ -21,9 +22,10 @@ class messages():
 def main():
     plt.ion()
 
-    fig, ax = plt.subplots(3, 3)
+    fig, ax = plt.subplots(4, 3)
     columnsFFT = ['Freq','FFTx', 'FFTy', 'FFTz']
     columnsAcc = ['Sample','ACCx', 'ACCy', 'ACCz']
+    columnsSpeed = ['Sample', 'Speed_x', 'Speed_y', 'Speed_z']
 
     dataFFT={'Freq':[0],
              'FFTx':[0.],
@@ -33,10 +35,16 @@ def main():
                'ACCx': [0.],
                'ACCy': [0.],
                'ACCz': [0.]}
+    dataSpeed = {'Sample': [0],
+               'Speed_x': [0.],
+               'Speed_y': [0.],
+               'Speed_z': [0.]}
 
     dfAcc = pd.DataFrame(data=dataAcc, columns=columnsAcc, index=dataAcc['Sample'])
 
     dfFFT = pd.DataFrame(data=dataFFT, columns=columnsFFT, index=dataFFT['Freq'])
+
+    dfSpeed = pd.DataFrame(data=dataSpeed, columns=columnsSpeed, index=dataSpeed['Sample'])
     #dfFFT.to_excel('FFT.xlsx')
     #dfAcc.to_excel('Acc.xlsx')
 
@@ -131,17 +139,19 @@ def main():
                         #plt.show()
                         #dfFFT.dropna(how='any',axis=0,inplace=True)
                         #print(dfFFT)
-                        dfFFT.to_excel('FFT.xlsx')
+                        #dfFFT.to_excel('FFT.xlsx')
                         #print(dfFFT)
 
 
 
 
 
-                if (fields[0] == 't'):
 
+
+                if (fields[0] == 't_a'):
+                    #print('t_a\n')
                     try:
-                        sample = float(fields[1])*aq_pr
+                        sample = float(fields[1]) * aq_pr
                     except:
                         sample = 0
 
@@ -165,40 +175,88 @@ def main():
                                    'ACCy': [accy],
                                    'ACCz': [accz]}
                     dfAcctemp = pd.DataFrame(data=dataAcctemp, columns=columnsAcc, index=dataAcctemp['Sample'])
-                    #dfAcc = pd.concat([dfAcctemp, dfAcc])
+                    # dfAcc = pd.concat([dfAcctemp, dfAcc])
                     dfAcc = dfAcc.append(dfAcctemp)
                     dfAcctemp = None
-                    #print(dfFFTtemp)
-                    #exit(1)
+                    # print(dfFFTtemp)
+                    # exit(1)
 
-                    if (int(fields[1])>=500):
+
+                if (fields[0] == 't_s'):
+                    #print('t_s\n')
+                    try:
+                        sample = float(fields[1]) * aq_pr
+                    except:
+                        sample = 0
+
+                    try:
+                        speed_x = float(fields[3])
+                    except:
+                        speed_x = 0
+
+                    try:
+                        speed_y = float(fields[5])
+                    except:
+                        speed_y = 0
+
+                    try:
+                        speed_z = float(fields[7])
+                    except:
+                        speed_z = 0
+
+                    dataSpeedtemp = {'Sample': [sample],
+                                     'Speed_x': [speed_x],
+                                     'Speed_y': [speed_y],
+                                     'Speed_z': [speed_z]}
+                    dfSpeedtemp = pd.DataFrame(data=dataSpeedtemp, columns=columnsSpeed,
+                                               index=dataSpeedtemp['Sample'])
+                    # dfAcc = pd.concat([dfAcctemp, dfAcc])
+                    dfSpeed = dfSpeed.append(dfSpeedtemp)
+                    dfSpeedtemp = None
+                    # print(dfFFTtemp)
+                    # exit(1)
+
+
+                    if (fields[0] == 't_s' and int(fields[1])>=500):
+                        print(f't_s: {fields[1]}\n')
+
                         #dfAcc.dropna(how='any',axis=0,inplace=True)
                         #print(dfAcc)
                         dfAcc.dropna(how='any',axis=0,inplace=True)
                         flagstartFFT = False
                         plt.clf()
                         fig.suptitle(f'Dados de amostragem:freq:{aq_fr} Per: {aq_pr} Tau: {aq_tau}')
-                        plt.subplot(3,3,1)
+                        plt.subplot(4,3,1)
                         plt.title('Eixo X')
                         plt.plot(dfAcc['ACCx'])
 
-                        plt.subplot(3, 3, 2)
+                        plt.subplot(4, 3, 2)
                         plt.title('Eixo Y')
                         plt.plot(dfAcc['ACCy'])
 
-                        plt.subplot(3, 3, 3)
+                        plt.subplot(4, 3, 3)
                         plt.title('Eixo Z')
                         plt.plot(dfAcc['ACCz'])
 
-                        plt.subplot(3, 3, 4)
+                        plt.subplot(4, 3, 4)
+                        plt.plot(dfSpeed['Speed_x'])
+
+                        plt.subplot(4, 3, 5)
+                        plt.plot(dfSpeed['Speed_y'])
+
+                        plt.subplot(4, 3, 6)
+                        plt.plot(dfSpeed['Speed_z'])
+
+
+                        plt.subplot(4, 3, 7)
                         #plt.title('FFTx')
                         plt.plot(dfFFT['FFTx'])
 
-                        plt.subplot(3, 3, 5)
+                        plt.subplot(4, 3, 8)
                         #plt.title('FFTy')
                         plt.plot(dfFFT['FFTy'])
 
-                        plt.subplot(3, 3, 6)
+                        plt.subplot(4, 3, 9)
                         #plt.title('FFTz')
                         plt.plot(dfFFT['FFTz'])
 
@@ -207,40 +265,44 @@ def main():
                         spx = np.fft.fft(dfAcc['ACCx'])
                         spModx = np.power(spx.real**2 + spx.imag**2,0.5)
                         spModHalfx = spModx[0:int(len(spModx)/2)]
-                        plt.subplot(3, 3, 7)
+                        plt.subplot(4, 3, 10)
                         #plt.title('FFTx-P')
                         plt.plot(spModHalfx)
 
                         spy = np.fft.fft(dfAcc['ACCy'])
                         spMody = np.power(spy.real ** 2 + spy.imag ** 2, 0.5)
                         spModHalfy = spMody[0:int(len(spMody) / 2)]
-                        plt.subplot(3, 3, 8)
+                        plt.subplot(4, 3, 11)
                         #plt.title('FFTy-P')
                         plt.plot(spModHalfy)
 
                         spz = np.fft.fft(dfAcc['ACCz'])
                         spModz = np.power(spz.real ** 2 + spz.imag ** 2, 0.5)
                         spModHalfz = spModz[0:int(len(spModz) / 2)]
-                        plt.subplot(3, 3, 9)
+                        plt.subplot(4, 3, 12)
                         #plt.title('FFTz-P')
                         plt.plot(spModHalfz)
 
                         #print(spModHalf)
                         #print(spMod)
 
-
-
+                        time_stamp = f'{datetime.now().year}-{datetime.now().month}-{datetime.now().day}_'
+                        f'{datetime.now().hour}_{datetime.now().minute}_{datetime.now().second}_a{i}'
                         #print(dfFFT)
                         #print(dfAcc)
                         plt.show()
-                        plt.savefig(f'Grafs\\Grafs_{j}.png')
+                        plt.savefig(f'Grafs\\Grafs_{time_stamp}.png')
                         fig.canvas.draw()
                         fig.canvas.flush_events()
-                        dfFFT.to_excel(f'FFT\\FFT{j}.xlsx')
-                        dfAcc.to_excel(f'ACC\\ACC{j}.xlsx')
+
+                        dfFFT.to_excel(f'FFT\\FFT_{time_stamp}.xlsx')
+
+                        dfSpeed.to_excel(f'Speed\\Speed_{time_stamp}.xlsx')
+
+                        dfAcc.to_excel(f'ACC\\ACC_{time_stamp}_a{i}.xlsx')
                         j+=1
                         dfAcc = pd.DataFrame(data=dataAcc, columns=columnsAcc, index=dataAcc['Sample'])
-
+                        dfSpeed = pd.DataFrame(data=dataSpeed, columns=columnsSpeed, index=dataSpeed['Sample'])
                         dfFFT = pd.DataFrame(data=dataFFT, columns=columnsFFT, index=dataFFT['Freq'])
                         #buttonPressed = False
                         #while (not buttonPressed):
