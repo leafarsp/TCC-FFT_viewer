669c6e5fe737e1fd6403029cef40d20dc0d8fb02
leafarsp <leafarsp@gmail.com>
2021-10-04 01:53:34 -0300

    Criadas rotinas para visualizar a FFT da velocidade

--- FFT_viewer.py
+++ FFT_viewer.py
@@ -6,6 +6,9 @@ import pandas as pd
 import sys
 from datetime import datetime
 import re
+import os
+ROOT_DIR = os.path.abspath(os.curdir)
+
 
 
 
@@ -68,6 +71,7 @@ def main():
     aq_fr=0.
     aq_fr = 0.
     aq_tau=0.
+    cont_fr_samples = 0
     if ser.is_open == True:
         while (line != "b'-1\n'"):
             line=ser.readline()
@@ -75,7 +79,7 @@ def main():
                 replace('\'', ''). \
                 replace('\\','').\
                 replace('n','').split(',')
-            #print(fields)
+            print(fields)
             #print(fields)
             if fields[0] == 'Dados de aquisicao':
                 try:
@@ -91,11 +95,56 @@ def main():
                     aq_tau = float(fields[6])
                 except:
                     aq_tau = 0.
+            if fields[0] == 'RMS-ACC':
+                try:
+                    rms_acc_x = float(fields[2])
+                except:
+                    rms_acc_x=0.
+                try:
+                    rms_acc_y = float(fields[4])
+                except:
+                    rms_acc_y = 0.
+
+                try:
+                    rms_acc_z = float(fields[6])
+                except:
+                    rms_acc_z = 0.
+
+            if fields[0] == 'RMS-Speed':
+                try:
+                    rms_speed_x = float(fields[2])
+                except:
+                    rms_speed_x=0.
+                try:
+                    rms_speed_y = float(fields[4])
+                except:
+                    rms_speed_y = 0.
+
+                try:
+                    rms_speed_z = float(fields[6])
+                except:
+                    rms_speed_z = 0.
+
+            if fields[0] == 'Pico-ACC':
+                try:
+                    pico_acc_x = float(fields[2])
+                except:
+                    pico_acc_x=0.
+                try:
+                    pico_acc_y = float(fields[4])
+                except:
+                    pico_acc_y = 0.
+
+                try:
+                    pico_acc_z = float(fields[6])
+                except:
+                    pico_acc_z = 0.
 
             if flagstartFFT:
                 #print(fields[0])
                 if (fields[0]=='Fr'):
                     #print(fields[0])
+                    cont_fr_samples += 1
                     try:
                         freq = float(fields[1])
                     except:
@@ -133,7 +182,8 @@ def main():
                     dfFFTtemp = None
                     #print(dfFFTtemp)
                     #exit(1)
-                    if (int(fields[1])>=255):
+                    if (cont_fr_samples >=(255)):
+                        cont_fr_samples = 0
                         #flagstartFFT = False
                         dfFFT.dropna(how='any',axis=0,inplace=True)
                         #plt.show()
@@ -194,6 +244,7 @@ def main():
                     except:
                         speed_x = 0
 
+
                     try:
                         speed_y = float(fields[5])
                     except:
@@ -220,86 +271,140 @@ def main():
                     if (fields[0] == 't_s' and int(fields[1])>=500):
                         print(f't_s: {fields[1]}\n')
 
+                        dfAcc.drop(axis=0,index=0,inplace=True)
+                        dfFFT.drop(axis=0,index=0,inplace=True)
+                        dfSpeed.drop(axis=0,index=0,inplace=True)
+
+                        subplot_col = 3
+                        subplot_lin = 4
+
                         #dfAcc.dropna(how='any',axis=0,inplace=True)
                         #print(dfAcc)
                         dfAcc.dropna(how='any',axis=0,inplace=True)
                         flagstartFFT = False
                         plt.clf()
-                        fig.suptitle(f'Dados de amostragem:freq:{aq_fr} Per: {aq_pr} Tau: {aq_tau}')
-                        plt.subplot(4,3,1)
+                        fig.suptitle(f'freq:{aq_fr:.2f}Hz Per: {aq_pr*1000:.3f}ms Tau: {aq_tau:.3}s')
+                        plt.text(f'RMS_vel[mm/s]:x={rms_speed_x:.2f} y={rms_speed_y:.2f} z={rms_speed_z:.2}'
+                                     f'RMS_acc[m/s^2]:x={rms_acc_x:.2f} y={rms_acc_y:.2f} z={rms_acc_z:.2}'
+                                     f'Pico_acc[m/s^2]:x={pico_acc_x:.2f} y={pico_acc_y:.2f} z={pico_acc_z:.2}',y=-10,s=10)
+                        plt.subplot(subplot_lin,subplot_col,1)
                         plt.title('Eixo X')
                         plt.plot(dfAcc['ACCx'])
 
-                        plt.subplot(4, 3, 2)
+                        plt.subplot(subplot_lin, subplot_col, 2)
                         plt.title('Eixo Y')
                         plt.plot(dfAcc['ACCy'])
 
-                        plt.subplot(4, 3, 3)
+                        plt.subplot(subplot_lin, subplot_col, 3)
                         plt.title('Eixo Z')
                         plt.plot(dfAcc['ACCz'])
 
-                        plt.subplot(4, 3, 4)
+                        plt.subplot(subplot_lin, subplot_col, 4)
                         plt.plot(dfSpeed['Speed_x'])
 
-                        plt.subplot(4, 3, 5)
+                        plt.subplot(subplot_lin, subplot_col, 5)
                         plt.plot(dfSpeed['Speed_y'])
 
-                        plt.subplot(4, 3, 6)
+                        plt.subplot(subplot_lin, subplot_col, 6)
                         plt.plot(dfSpeed['Speed_z'])
 
 
-                        plt.subplot(4, 3, 7)
+                        plt.subplot(subplot_lin, subplot_col, 7)
                         #plt.title('FFTx')
                         plt.plot(dfFFT['FFTx'])
 
-                        plt.subplot(4, 3, 8)
+                        plt.subplot(subplot_lin, subplot_col, 8)
                         #plt.title('FFTy')
                         plt.plot(dfFFT['FFTy'])
 
-                        plt.subplot(4, 3, 9)
+                        plt.subplot(subplot_lin, subplot_col, 9)
                         #plt.title('FFTz')
                         plt.plot(dfFFT['FFTz'])
 
+                        qt_amostras = (dfAcc['Sample']).count()
+                        T_amostragem = max(dfAcc['Sample'])
 
+                        fft_beams = 512
 
-                        spx = np.fft.fft(dfAcc['ACCx'])
-                        spModx = np.power(spx.real**2 + spx.imag**2,0.5)
+                        f_res = (qt_amostras / T_amostragem) / fft_beams
+                        #print(f'frequency resolution = {f_res}Hz')
+
+                        f_range = np.linspace(0, (fft_beams / 2) * f_res, int(fft_beams / 2))
+
+                        #spx = np.fft.fft(dfSpeed['Speed_x'],n=fft_beams)
+                        #spy = np.fft.fft(dfSpeed['Speed_y'],n=fft_beams)
+                        #spz = np.fft.fft(dfSpeed['Speed_z'],n=fft_beams)
+
+                        spx = np.fft.fft(dfAcc['ACCx'], n=fft_beams)
+                        spy = np.fft.fft(dfAcc['ACCy'], n=fft_beams)
+                        spz = np.fft.fft(dfAcc['ACCz'], n=fft_beams)
+
+                        spModx = np.abs(spx)
                         spModHalfx = spModx[0:int(len(spModx)/2)]
-                        plt.subplot(4, 3, 10)
+                        spModHalfx = spModHalfx / (qt_amostras/4)
+
+                        plt.subplot(subplot_lin, subplot_col, 10)
                         #plt.title('FFTx-P')
-                        plt.plot(spModHalfx)
+                        print(f'f_range={len(f_range)},spModHalfx={len(spModHalfx)}')
+                        plt.plot(f_range,spModHalfx)
 
-                        spy = np.fft.fft(dfAcc['ACCy'])
-                        spMody = np.power(spy.real ** 2 + spy.imag ** 2, 0.5)
+
+                        spMody = np.abs(spy)
                         spModHalfy = spMody[0:int(len(spMody) / 2)]
-                        plt.subplot(4, 3, 11)
+                        spModHalfy = spModHalfy / (qt_amostras/4)
+                        plt.subplot(subplot_lin, subplot_col, 11)
                         #plt.title('FFTy-P')
-                        plt.plot(spModHalfy)
+                        print(f'f_range={len(f_range)},spModHalfx={len(spModHalfy)}')
+                        plt.plot(f_range, spModHalfy)
 
-                        spz = np.fft.fft(dfAcc['ACCz'])
-                        spModz = np.power(spz.real ** 2 + spz.imag ** 2, 0.5)
+
+
+
+                        spModz = np.abs(spz)
                         spModHalfz = spModz[0:int(len(spModz) / 2)]
-                        plt.subplot(4, 3, 12)
+                        spModHalfz = spModHalfz / (qt_amostras/4)
+                        plt.subplot(subplot_lin, subplot_col, 12)
                         #plt.title('FFTz-P')
-                        plt.plot(spModHalfz)
+                        plt.plot(f_range,spModHalfz)
 
+                        """"Verificação do cálculo de velocidade feito pela placa
+                        #speed_x_calc = dfAcc['ACCx']
+                        df_speed_x = integrate_acc_numerically(dfAcc['ACCx'], 'ACCx', 'Speed_x')
+                        plt.subplot(subplot_lin, subplot_col, 13)
+                        plt.plot(df_speed_x['Speed_x'])
+
+                        df_speed_y = integrate_acc_numerically(dfAcc['ACCy'], 'ACCy', 'Speed_y')
+                        plt.subplot(subplot_lin, subplot_col, 14)
+                        plt.plot(df_speed_y['Speed_y'])
+
+                        df_speed_z = integrate_acc_numerically(dfAcc['ACCz'], 'ACCz', 'Speed_z')
+                        plt.subplot(subplot_lin, subplot_col, 15)
+                        plt.plot(df_speed_z['Speed_z'])
+                        """
                         #print(spModHalf)
                         #print(spMod)
 
-                        time_stamp = f'{datetime.now().year}-{datetime.now().month}-{datetime.now().day}_'
-                        f'{datetime.now().hour}_{datetime.now().minute}_{datetime.now().second}_a{i}'
+                        time_stamp = f'{datetime.now().year}-{datetime.now().month}-{datetime.now().day}_' \
+                                     f'{datetime.now().hour}_{datetime.now().minute}_{datetime.now().second}_a{j}'
                         #print(dfFFT)
                         #print(dfAcc)
+
+
+                        plt.savefig(f'{ROOT_DIR}\\Grafs\\Grafs_{time_stamp}.png')
+                        dfFFT.to_excel(f'{ROOT_DIR}\\FFT\\FFT_{time_stamp}.xlsx')
+
+                        dfSpeed.to_excel(f'{ROOT_DIR}\\Speed\\Speed_{time_stamp}.xlsx')
+
+                        dfAcc.to_excel(f'{ROOT_DIR}\\ACC\\ACC_{time_stamp}.xlsx')
                         plt.show()
-                        plt.savefig(f'Grafs\\Grafs_{time_stamp}.png')
+
                         fig.canvas.draw()
                         fig.canvas.flush_events()
 
-                        dfFFT.to_excel(f'FFT\\FFT_{time_stamp}.xlsx')
 
-                        dfSpeed.to_excel(f'Speed\\Speed_{time_stamp}.xlsx')
 
-                        dfAcc.to_excel(f'ACC\\ACC_{time_stamp}_a{i}.xlsx')
+
+
                         j+=1
                         dfAcc = pd.DataFrame(data=dataAcc, columns=columnsAcc, index=dataAcc['Sample'])
                         dfSpeed = pd.DataFrame(data=dataSpeed, columns=columnsSpeed, index=dataSpeed['Sample'])
@@ -312,6 +417,7 @@ def main():
             #print(fields[0])
             if (fields[0] == 'FFT:'):
                 flagstartFFT=True
+                cont_fr_samples = 0
 
 
 
@@ -350,6 +456,22 @@ def ask_for_port():
         else:
             port = ports[index]
         return port
+
+
+def integrate_acc_numerically(df, eixo_acc, eixo_speed):
+    ret_df = pd.DataFrame(data = None,index = df.index, columns = ['t_s',eixo_speed])
+    ret_df.iloc[0][1]=0
+
+    for i in range(1, df.index.size):
+        #print(f'i={i} df.iloc[i]={df.iloc[i]} df.iloc[i-1]={df.iloc[i-1]} df.index[i]={df.index[i]} '
+              #f'df.index[i-1]={df.index[i-1]} ret_df.iloc[i][1]={ret_df.iloc[i-1][1]}')
+        dt = (df.index[i]-df.index[i-1])
+        ret_df.iloc[i] = (((df.iloc[i]+df.iloc[i-1])/2) * dt)*1000. + ret_df.iloc[i-1][1]
+
+
+    ret_df.iloc[0][1] = ret_df.iloc[1][1]
+    ret_df[eixo_speed] = ret_df[eixo_speed] - ret_df[eixo_speed].mean()
+    return ret_df
 
 
 
